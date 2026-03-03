ccd81256cd58c7b2d5e8cb6050d5cd92ea077adb
EdilenoSantos <102782268+EdilenoSantos@users.noreply.github.com>
2026-03-03 14:36:24 -0300

    Fix bug in power calculation and comment out code
    Comment out the polarResult calculation and add a bug fix note for b^(pi*i) returning -1.

--- sources/power.ts
+++ sources/power.ts
@@ -468,12 +468,19 @@ function yypower(base: U, exponent: U): U {
     }
   }
 
-  const polarResult = simplify_polar(exponent);
+  // FIX BUG: b^(pi*i) => -1, for b != e
+  /*const polarResult = simplify_polar(exponent);
   if (polarResult !== undefined) {
     if (DEBUG_POWER) {
       console.log('   power: using simplify_polar');
     }
     return polarResult;
+  }*/
+  if (base === symbol(E)) {
+    const polarResult = simplify_polar(exponent);
+    if (polarResult !== undefined) {
+      return polarResult;
+    }
   }
 
   const result = makeList(symbol(POWER), base, exponent);
